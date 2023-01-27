a116a2f7693133d08fc6ca28d28f06ee168d9c06
peters04 <piotreksmolinski04@gmail.com>
2023-01-28 00:24:41 +0100

    help

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer");
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
-  },
-});
+module.exports = {
+  transporter: nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  }),
+};
